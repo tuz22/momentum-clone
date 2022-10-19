2f6bb1f522843b382e1e6b592b30732954c9098d
tuz22 <seungji.dev@gmail.com>
2022-10-19 16:01:06 +0900

    투두리스트 삭제버튼 추가

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,22 @@ const toDoForm = document.querySelector('#todoForm');
 const toDoInput = document.querySelector('#todoForm input');
 const toDoList = document.querySelector('#todoList');
 
+function deleteToDo(e){
+  const li = e.target.parentElement;
+  li.remove();
+}
+
 function writeToDo(item){
   const li = document.createElement('li');
   const span = document.createElement('span');
+  const button = document.createElement('button');
 
   li.appendChild(span);
+  li.appendChild(button);
   span.innerHTML = item;
+  button.innerHTML = 'X';
+
+  button.addEventListener('click', deleteToDo);
   toDoList.appendChild(li);
 }
 
@@ -15,6 +25,7 @@ function handleToDoSubmit(e){
   e.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+
   writeToDo(newTodo);
 }
 toDoForm.addEventListener('submit', handleToDoSubmit);
